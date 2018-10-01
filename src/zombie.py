1252e6e92365e02d18f7971402d74fa20689a18a
romain.fontugne <romain.fontugne@gmail.com>
2018-10-01 18:04:39 +0900

    add list of real zombie period

--- src/zombie.py
+++ src/zombie.py
@@ -57,16 +57,16 @@ def getBGPdata( params ):
         logging.warning("Already got BGP data: {}, {}".format(etime, prefixes))
 
 # 2018/7 and 2018/9
-starttime = 1531958400
-endtime = 1537401600
+#starttime = 1531958400
+#endtime = 1537401600
 
 # 2017/10 and 2017/12
 #starttime = 1506816000
 #endtime = 1514764800
 
 # 2017/03 and 2017/04
-#starttime = 1488326400
-#endtime = 1493596800
+starttime = 1488326400
+endtime = 1493596800
 
 
 FORMAT = '%(asctime)s %(processName)s %(message)s'
