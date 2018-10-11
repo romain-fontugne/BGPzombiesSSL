d51b75a10ffb1aa0fb3673dc20f71a3d82d25a2e
romain.fontugne <romain.fontugne@gmail.com>
2018-10-11 19:45:06 +0900

    sync

--- src/zombie_stats.py
+++ src/zombie_stats.py
@@ -166,175 +166,185 @@ def get_classification_results(ts = 1505287800, prefix = "84.205.67.0/24"):
     return {"zombie": zombie_asns, "normal": normal_asns}
 
 
-def compute_all_stats(af=4):
+def compute_all_stats():
     """ Fetch all classification results and compute basic stats."""
 
+    h_x = []
+    h_label = []
+
     ### Fetch all results
-    all_classification_results = defaultdict(dict)
+    for af, pfx_len in [(4, "24"),(6, "48")]:
+        all_classification_results = defaultdict(dict)
 
-    if af == 4:
-        pfx_len = "24"
-    else:
-        pfx_len = "48"
+        for path in glob.glob(esteban_results_directory+"/*_"+pfx_len):
+            dname = path.rpartition("/")[2]
+            ts, _, prefix = dname.partition("_")
+            prefix = prefix.replace("_","/")
+            ts = int(ts)
 
-    for path in glob.glob(esteban_results_directory+"/*_"+pfx_len):
-        dname = path.rpartition("/")[2]
-        ts, _, prefix = dname.partition("_")
-        prefix = prefix.replace("_","/")
-        ts = int(ts)
+            # print("Processing %s %s..." % (ts, prefix))
+            asns = get_classification_results(ts, prefix)
+            if asns is not None and len(asns["zombie"])>200:
+                print("Large outbreak: {} {}".format(ts, prefix))
 
-        # print("Processing %s %s..." % (ts, prefix))
-        asns = get_classification_results(ts, prefix)
-        if asns is not None and len(asns["zombie"])>200:
-            print("Large outbreak: {} {}".format(ts, prefix))
+            if asns is not None:
+                all_classification_results[ts][prefix] = asns
 
-        if asns is not None:
-            all_classification_results[ts][prefix] = asns
+        ### Temporal characteristics
+        ts_start = min(all_classification_results.keys())
+        ts_end = max(all_classification_results.keys())
+        # duration = (ts_end - ts_start)/3600 #hours
+        # nb_withdraws = duration/4
+        duration = 31+28+31+28+13+31 #days
+        nb_withdraws = duration*6
+        
+        print("First zombie detected at {} and last one at {}".format(
+            datetime.datetime.utcfromtimestamp(ts_start),
+            datetime.datetime.utcfromtimestamp(ts_end)
+            ))
+        
+        nb_zombie_timebin = len(all_classification_results.keys())
 
-    ### Temporal characteristics
-    ts_start = min(all_classification_results.keys())
-    ts_end = max(all_classification_results.keys())
-    # duration = (ts_end - ts_start)/3600 #hours
-    # nb_withdraws = duration/4
-    duration = 31+28+31+28+13+31 #days
-    nb_withdraws = duration*6
-    
-    print("First zombie detected at {} and last one at {}".format(
-        datetime.datetime.utcfromtimestamp(ts_start),
-        datetime.datetime.utcfromtimestamp(ts_end)
-        ))
-    
-    nb_zombie_timebin = len(all_classification_results.keys())
+        perc_zombie_timebins = nb_zombie_timebin/nb_withdraws*100
+        print("Percentage of withdraw periods with at least one zombie: {:.02f}%".format(perc_zombie_timebins))
+        print("Number of withdraw periods with zombies: {}".format(len(all_classification_results)))
 
-    perc_zombie_timebins = nb_zombie_timebin/nb_withdraws*100
-    print("Percentage of withdraw periods with at least one zombie: {:.02f}%".format(perc_zombie_timebins))
-    print("Number of withdraw periods with zombies: {}".format(len(all_classification_results)))
+        zombie_timebins_v4 = [ts for ts, res in all_classification_results.items() for pfx in res.keys() if "." in pfx]
+        zombie_timebins_v6 = [ts for ts, res in all_classification_results.items() for pfx in res.keys() if ":" in pfx]
+        perc_zombie_timebins_v4 = len(set(zombie_timebins_v4))/nb_withdraws*100
+        perc_zombie_timebins_v6 = len(set(zombie_timebins_v6))/nb_withdraws*100
+        print("Percentage of withdraw periods with one v4 zombie: {:.02f}%".format(perc_zombie_timebins_v4))
+        print("Percentage of withdraw periods with one v6 zombie: {:.02f}%".format(perc_zombie_timebins_v6))
 
-    zombie_timebins_v4 = [ts for ts, res in all_classification_results.items() for pfx in res.keys() if "." in pfx]
-    zombie_timebins_v6 = [ts for ts, res in all_classification_results.items() for pfx in res.keys() if ":" in pfx]
-    perc_zombie_timebins_v4 = len(set(zombie_timebins_v4))/nb_withdraws*100
-    perc_zombie_timebins_v6 = len(set(zombie_timebins_v6))/nb_withdraws*100
-    print("Percentage of withdraw periods with one v4 zombie: {:.02f}%".format(perc_zombie_timebins_v4))
-    print("Percentage of withdraw periods with one v6 zombie: {:.02f}%".format(perc_zombie_timebins_v6))
-
-    zombies_per_timebin = [set([asn for pfx, res in pfx_res.items() for asn in res["zombie"]]) 
-            for ts, pfx_res in all_classification_results.items()]
-    normal_per_timebin = [set([asn for pfx, res in pfx_res.items() for asn in res["normal"]]) 
-            for ts, pfx_res in all_classification_results.items()]
+        zombies_per_timebin = [set([asn for pfx, res in pfx_res.items() for asn in res["zombie"]]) 
+                for ts, pfx_res in all_classification_results.items()]
+        normal_per_timebin = [set([asn for pfx, res in pfx_res.items() for asn in res["normal"]]) 
+                for ts, pfx_res in all_classification_results.items()]
 
 
-    nb_zombie_per_outbreak = [len(z) for z in zombies_per_timebin]
-    print("On average we have {:.02f} zombie AS per outbreak (median={})".format(np.mean(nb_zombie_per_outbreak),np.median(nb_zombie_per_outbreak)))
-    print("On average we have {:.02f} AS in the AS graph".format(
-        np.mean(
-            [len(z.union(n)) for z,n in zip(zombies_per_timebin, normal_per_timebin)]
-        )))
-    print("That's {:.02f}% zombie AS per outbreak".format(
-        np.mean(
-            [100*len(z)/(len(z)+len(n)) for z,n in zip(zombies_per_timebin, normal_per_timebin)]
-        )))
+        nb_zombie_per_outbreak = [len(z) for z in zombies_per_timebin]
+        print("On average we have {:.02f} zombie AS per outbreak (median={})".format(np.mean(nb_zombie_per_outbreak),np.median(nb_zombie_per_outbreak)))
+        print("On average we have {:.02f} AS in the AS graph".format(
+            np.mean(
+                [len(z.union(n)) for z,n in zip(zombies_per_timebin, normal_per_timebin)]
+            )))
+        print("That's {:.02f}% zombie AS per outbreak".format(
+            np.mean(
+                [100*len(z)/(len(z)+len(n)) for z,n in zip(zombies_per_timebin, normal_per_timebin)]
+            )))
 
-    plt.figure(1)
-    rfplt.ecdf(nb_zombie_per_outbreak, label="IPv{}".format(af))
-    plt.xlabel("Number zombie ASN per outbreak")
-    plt.ylabel("CDF")
-    plt.legend(loc="best")
-    plt.tight_layout()
-    plt.savefig("fig/CDF_nb_zombie_per_outbreak.pdf")
+        plt.figure(1)
+        rfplt.ecdf(nb_zombie_per_outbreak, label="IPv{}".format(af))
+        plt.xlabel("Number zombie ASN per outbreak")
+        plt.ylabel("CDF")
+        plt.legend(loc="best")
+        plt.tight_layout()
+        plt.savefig("fig/CDF_nb_zombie_per_outbreak.pdf")
 
 
-### Zombie Frequency for all beacons
-    asn_zombie_frequency = collections.Counter(itertools.chain.from_iterable(zombies_per_timebin))
-    # add normal ASes:
-    for asn in set(itertools.chain.from_iterable(normal_per_timebin)):
-        if not asn in asn_zombie_frequency:
-            asn_zombie_frequency[asn]=0
-
-    print("Top 50 zombie ASN: ")
-    for asn, freq in asn_zombie_frequency.most_common(50):
-        print("\t AS{}: {:.02f}% ({} times)".format(asn, 100*freq/nb_zombie_timebin, freq))
-
-    plt.figure(2)
-    rfplt.ecdf(np.array(list(asn_zombie_frequency.values()))/nb_zombie_timebin, label="IPv{}".format(af))
-    plt.xlabel("freq. AS as zombie/total nb. of outbreaks")
-    plt.ylabel("CDF")
-    plt.legend(loc="best")
-    plt.tight_layout()
-    plt.savefig("fig/CDF_zombie_freq_per_asn.pdf")
-    # plt.show()
-
-### Zombie frequency per beacon
-    all_beacons = set([pfx for pfx_res in all_classification_results.values() for pfx in pfx_res.keys()])
-    plt.figure(3)
-    print(all_beacons)
-    for pfx in all_beacons: 
-        zombies_per_timebin_per_beacon = [set([asn for asn in pfx_res[pfx]["zombie"]]) 
-            for ts, pfx_res in all_classification_results.items() if pfx in pfx_res]
-
-        asn_zombie_frequency = collections.Counter(itertools.chain.from_iterable(zombies_per_timebin_per_beacon))
+    ### Zombie Frequency for all beacons
+        asn_zombie_frequency = collections.Counter(itertools.chain.from_iterable(zombies_per_timebin))
         # add normal ASes:
         for asn in set(itertools.chain.from_iterable(normal_per_timebin)):
             if not asn in asn_zombie_frequency:
                 asn_zombie_frequency[asn]=0
 
-        print("Top 10 zombie ASN for {}: ".format(pfx))
-        for asn, freq in asn_zombie_frequency.most_common(10):
-            print("\t AS{}: {:.02f}% ({} times)".format(asn, 100*freq/len(zombies_per_timebin_per_beacon), freq))
+        # print("Top 50 zombie ASN: ")
+        # for asn, freq in asn_zombie_frequency.most_common(50):
+            # print("\t AS{}: {:.02f}% ({} times)".format(asn, 100*freq/nb_zombie_timebin, freq))
 
-        rfplt.ecdf(np.array(list(asn_zombie_frequency.values()))/len(zombies_per_timebin_per_beacon), label=pfx)
+        plt.figure(2)
+        rfplt.ecdf(np.array(list(asn_zombie_frequency.values()))/nb_zombie_timebin, label="IPv{}".format(af))
         plt.xlabel("freq. AS as zombie/total nb. of outbreaks")
         plt.ylabel("CDF")
-        plt.legend(loc="best", prop={"size":8}, ncol=2)
+        plt.legend(loc="best")
         plt.tight_layout()
-        plt.savefig("fig/CDF_zombie_freq_per_asn_per_beacon.pdf")
+        plt.savefig("fig/CDF_zombie_freq_per_asn.pdf")
         # plt.show()
 
+    ### Zombie frequency per beacon
+        all_beacons = set([pfx for pfx_res in all_classification_results.values() for pfx in pfx_res.keys()])
+        plt.figure(3)
+        print(all_beacons)
+        for pfx in all_beacons: 
+            zombies_per_timebin_per_beacon = [set([asn for asn in pfx_res[pfx]["zombie"]]) 
+                for ts, pfx_res in all_classification_results.items() if pfx in pfx_res]
+
+            asn_zombie_frequency = collections.Counter(itertools.chain.from_iterable(zombies_per_timebin_per_beacon))
+            # add normal ASes:
+            for asn in set(itertools.chain.from_iterable(normal_per_timebin)):
+                if not asn in asn_zombie_frequency:
+                    asn_zombie_frequency[asn]=0
+
+            # print("Top 10 zombie ASN for {}: ".format(pfx))
+            # for asn, freq in asn_zombie_frequency.most_common(10):
+                # print("\t AS{}: {:.02f}% ({} times)".format(asn, 100*freq/len(zombies_per_timebin_per_beacon), freq))
+
+            rfplt.ecdf(np.array(list(asn_zombie_frequency.values()))/len(zombies_per_timebin_per_beacon), label=pfx)
+            plt.xlabel("freq. AS as zombie/total nb. of outbreaks")
+            plt.ylabel("CDF")
+            plt.legend(loc="best", prop={"size":8}, ncol=2)
+            plt.tight_layout()
+            plt.savefig("fig/CDF_zombie_freq_per_asn_per_beacon.pdf")
+            # plt.show()
 
 
 
 
 
-    # from efficient_apriori import apriori
 
-    # itemsets, rules = apriori(zombies_per_timebin, min_support=0.3, min_confidence=1)
-    # print(zombies_per_timebin)
-    # for nb_elem, items in itemsets.items():
-        # if nb_elem > 1:
-            # print(nb_elem)
-            # print(items)
+        # from efficient_apriori import apriori
 
-    # rules_rhs = filter(lambda rule: len(rule.lhs)==1 and len(rule.rhs)>4, rules)
-    # print(list(rules_rhs))
+        # itemsets, rules = apriori(zombies_per_timebin, min_support=0.3, min_confidence=1)
+        # print(zombies_per_timebin)
+        # for nb_elem, items in itemsets.items():
+            # if nb_elem > 1:
+                # print(nb_elem)
+                # print(items)
 
-
-    nb_outbreak_per_prefix = defaultdict(int)
-    for ts, events in all_classification_results.items():
-        for prefix, classification in events.items():
-            nb_outbreak_per_prefix[prefix] += 1
-
-    print(nb_outbreak_per_prefix)
-    plt.figure(4)
-    plt.hist(list(nb_outbreak_per_prefix.values()), label="IPv{}".format(af))
-    plt.xlabel("Number of outbreaks per beacon")
-    plt.ylabel("Number of beacons")
-    plt.legend(loc="best")
-    plt.tight_layout()
-    plt.savefig("fig/hist_nb_outbreak_per_prefix.pdf")
+        # rules_rhs = filter(lambda rule: len(rule.lhs)==1 and len(rule.rhs)>4, rules)
+        # print(list(rules_rhs))
 
 
-    nb_beacon_per_ts = {ts: len(events) for ts, events in all_classification_results.items()}
-    # print(nb_beacon_per_ts)
+        nb_outbreak_per_prefix = defaultdict(int)
+        for ts, events in all_classification_results.items():
+            for prefix, classification in events.items():
+                nb_outbreak_per_prefix[prefix] += 1
 
-    plt.figure(5)
-    rfplt.ecdf(list(nb_beacon_per_ts.values()), label="IPv{}".format(af))
-    plt.xlabel("Number of simultaneous outbreaks")
-    plt.ylabel("CDF")
-    plt.legend(loc="best")
-    plt.tight_layout()
-    plt.savefig("fig/CDF_nb_simult_zombie.pdf")
+        # print(nb_outbreak_per_prefix)
+        h_values = list(nb_outbreak_per_prefix.values())
+        plt.figure()
+        h_x.append(h_values)
+        h_label.append("IPv{}".format(af))
+        plt.hist(h_x, label=h_label)
+        plt.xlabel("Number of outbreaks per beacon")
+        plt.ylabel("Number of beacons")
+        plt.legend(loc="best")
+        plt.tight_layout()
+        plt.savefig("fig/hist_nb_outbreak_per_prefix.pdf")
+
+        print("On average {} outbreaks per beacon (max={})".format(np.mean(h_values), np.max(h_values)))
+
+
+        nb_beacon_per_ts = {ts: len(events) for ts, events in all_classification_results.items()}
+        # print(nb_beacon_per_ts)
+
+        plt.figure(5)
+        cdf = rfplt.ecdf(list(nb_beacon_per_ts.values()), label="IPv{}".format(af))
+        plt.xlabel("Number of simultaneous outbreaks")
+        plt.ylabel("CDF")
+        plt.legend(loc="best")
+        plt.xlim([0.5,14.5])
+        plt.tight_layout()
+        plt.savefig("fig/CDF_nb_simult_zombie.pdf")
+        print(cdf)
+        print(max(nb_beacon_per_ts, key=nb_beacon_per_ts.get))
+
+        for ts, val in nb_beacon_per_ts.items():
+            if val==13:
+                print(ts)
 
 if __name__ == "__main__":
-    compute_all_stats(4)    
-    compute_all_stats(6)    
+    compute_all_stats()    
     pathLenComparions()
     peerZombieLikelihood()
